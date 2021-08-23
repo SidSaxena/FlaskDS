4077dc0d0d211067d2d5baa1a8e9492cf98f25fc
Sid <saxena.siddhu@gmail.com>
2021-08-24 00:06:59 +0530

    implemented flask server script

---
+++ server.py
@@ -0,0 +1,125 @@
+from sqlite3 import Connection as SQLite3Connection
+from datetime import datetime as dt
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import linked_list
+import random
+
+# app
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+
+# configure sqlite3 to enforce foreign key constraints
+@event.listens_for(Engine, 'connect')
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+db = SQLAlchemy(app)
+now = dt.now()
+
+# models
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Columns(db.Integer, primary_key=True)
+    name = db.Columns(db.String(50))
+    email = db.Columns(db.String(50))
+    address = db.Columns(db.String(200))
+    phone = db.Columns(db.String(50))
+    posts = db.relationship("BlogPost", cascade='all, delete')
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(50))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+# routes
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name = data['name'],
+        email = data['email'],
+        address = data['address'],
+        phone = data['phone'],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"message": "User Created"}), 200
+
+@app.route("/user/desc_id", methods=["GET"])
+def get_all_users_desc():
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_beginning(
+            {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'address': user.address,
+                'phone': user.phone,
+            }
+        )
+
+    return jsonify(all_users_ll.to_list()), 200
+
+@app.route("/user/asc_id", methods=["GET"])
+def get_all_users_asc():
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_at_end(
+            {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'address': user.address,
+                'phone': user.phone,
+            }
+        )
+
+    return jsonify(all_users_ll.to_list()), 200
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    users = User.query.all()
+
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_beginning(
+            {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'address': user.address,
+                'phone': user.phone,
+            }
+        )
+    user = all_users_ll.get_user_by_id(user_id)
+
+    return jsonify(user), 200
+
+@app.route("/user/<user_id>", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
